a518f4fb57c68887f79d5cb0b3bb2b9829eadf65
Lucas Souza Duarte <lucas.souzaduarte.73@gmail.com>
2024-12-11 14:01:57 -0300

    feat: create user model code

--- mycommerce/user/models/user.py
+++ mycommerce/user/models/user.py
@@ -1,11 +1,49 @@
 from django.db import models
+from django.contrib.auth.models import BaseUserManager, AbstractBaseUser
 
-class User(models.Model):
-  email = models.CharField(max_length=254, unique=True)
-  password = models.CharField(max_length=128)
+class AccountManager(BaseUserManager):
+  def create_user(self, email, password=None, **kwargs):
+
+    if not email:
+      raise ValueError("Email is required")
+
+    email = self.normalize_email(email)
+    user = self.model(email=email, **kwargs)
+    user.set_password(password)
+    user.save(using=self.db)
+    return user
+  
+  def create_superuser(self, email, password=None, **kwargs):
+    user = self.create_user(
+      email=self.normalize_email(email),
+      password=password
+    )
+
+    user.is_admin = True
+    user.is_staff = True
+    user.is_superuser = True
+    user.save(using=self.db)
+    return
+  
+  def get_by_natural_key(self, email):
+        return self.get(email=email)
+
+class Account(AbstractBaseUser):
+  email = models.EmailField(max_length=254, unique=True)
+  first_name = models.CharField(max_length=128)
+  last_name = models.CharField(max_length=128)
+  profile_picture = models.ImageField(upload_to=None, blank=True, null=True)
   created_at = models.DateTimeField(auto_now_add=True)
   last_login = models.DateTimeField(blank=True, null=True)
+  is_admin = models.BooleanField(default=False)
   is_staff = models.BooleanField(default=False)
   is_superuser = models.BooleanField(default=False)
   is_active = models.BooleanField(default=True)
-  first_name = models.CharField()
+
+  objects = AccountManager()
+
+  USERNAME_FIELD = "email"
+  REQUIRED_FIELDS = []
+
+  def __str__(self):
+    return f"{self.first_name} {self.last_name}"
