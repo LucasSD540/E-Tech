869832e3d815f71110e5005321ee53cf17d9180e
Lucas Souza Duarte <lucas.souzaduarte.73@gmail.com>
2025-06-24 19:30:28 -0300

    add file

--- backend/mycommerce/settings.py
+++ backend/mycommerce/settings.py
@@ -80,7 +80,7 @@ WSGI_APPLICATION = 'mycommerce.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
+        'ENGINE': 'mysql.connector.django',
         'NAME': config('MYSQL_DATABASE'),
         'USER': config('MYSQL_USER'),
         'PASSWORD': config('MYSQL_PASSWORD'),
@@ -88,7 +88,6 @@ DATABASES = {
         "PORT": "3306",
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
